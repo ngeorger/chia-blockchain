cd524c2a7d9b84746486246a9bd028743bcf3429
matt <matt@chia.net>
2022-01-31 16:23:10 +0000

    fix get_hints()

--- chia/types/coin_spend.py
+++ chia/types/coin_spend.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import List
-
-from blspy import G2Element
+from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import SerializedProgram, INFINITE_COST
 from chia.types.condition_opcodes import ConditionOpcode
@@ -29,24 +28,14 @@ class CoinSpend(Streamable):
         return fee_for_solution(self.puzzle_reveal, self.solution, INFINITE_COST)
 
     def hints(self) -> List[bytes]:
-        # import above was causing circular import issue
-        from chia.full_node.mempool_check_conditions import get_name_puzzle_conditions
-        from chia.consensus.default_constants import DEFAULT_CONSTANTS
-        from chia.types.spend_bundle import SpendBundle
-        from chia.full_node.bundle_tools import simple_solution_generator
+        h_list = list()
+        puzzle = Program.from_bytes(bytes(self.puzzle_reveal))
+        solution = Program.from_bytes(bytes(self.solution))
+        cost, result = puzzle.run_with_cost(INFINITE_COST, solution)
+        for opcode_list in result.as_iter():
 
-        bundle = SpendBundle([self], G2Element())
-        generator = simple_solution_generator(bundle)
-
-        npc_result = get_name_puzzle_conditions(
-            generator, INFINITE_COST, cost_per_byte=DEFAULT_CONSTANTS.COST_PER_BYTE, mempool_mode=False
-        )
-        h_list = []
-        for npc in npc_result.npc_list:
-            for opcode, conditions in npc.conditions:
-                if opcode == ConditionOpcode.CREATE_COIN:
-                    for condition in conditions:
-                        if len(condition.vars) > 2 and condition.vars[2] != b"":
-                            h_list.append(condition.vars[2])
+            if opcode_list.first() == ConditionOpcode.CREATE_COIN and len(opcode_list.as_python()) > 3:
+                # (51 0xcafef00d 200 (hint))
+                h_list.append(opcode_list.rest().rest().rest().first().first().as_atom())
 
         return h_list
